8ca0179e079859e3694762feefb536bd7cd25c97
Ayyappan <ayyapanr@hotmail.com>
2020-06-20 07:22:55 +1000

    Progressive Web App

--- src/components/Robocard.js
+++ src/components/Robocard.js
@@ -4,7 +4,7 @@ const Robocard = (props) => {
         return (
             
                    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                    <img src={`https://robohash.org/${props.id}?size=525x525&bgset=any`} alt={props.id} />
+                    <img src={`https://robohash.org/${props.id}?size=200x200`} alt={props.id} />
                     <div>
                     <h2>{props.name}</h2>
                     <p>{props.email}</p>
