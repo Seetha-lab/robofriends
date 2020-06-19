2e1016d893f1ac2fb7565317826fea9289cd59bd
Ayyappan <ayyapanr@hotmail.com>
2020-06-20 07:47:59 +1000

    Progressive Web App

--- src/components/Robocard.js
+++ src/components/Robocard.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import AspectRatio from 'react-aspect-ratio';
 
 const Robocard = (props) => {
         return (
             
                    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                    <img src={`https://robohash.org/${props.id}?size=525x525&bgset=any`} alt={props.id} />
-                    <div>
+                         <AspectRatio ratio="3/4" style={{ maxWidth: '400px' }}>
+                        <img src={`https://robohash.org/${props.id}`} alt={props.id} />
+                            </AspectRatio>
+                                       <div>
                     <h2>{props.name}</h2>
                     <p>{props.email}</p>
                     </div>
